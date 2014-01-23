98477b02c70ff066b618436fd75972b9d4f3fd8b
queler@gmail.com <queler@gmail.com@53b26c66-57f6-ec13-a3ad-521236108fec>
2014-01-23 22:30:20 +0000

    retry: looks to be working, need to test on UL but RC

--- branches/syncing/Form2.designer.cs
+++ branches/syncing/Form2.designer.cs
@@ -61,6 +61,7 @@ namespace PTASync
 			this.textBox1.Location = new System.Drawing.Point(0, 0);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
 			this.textBox1.Size = new System.Drawing.Size(605, 451);
 			this.textBox1.TabIndex = 2;
 			// 
